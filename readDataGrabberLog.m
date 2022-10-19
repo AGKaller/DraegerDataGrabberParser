f4e29d5deff65269602286108e818a86afd50e49
f-k-s <koschumacher@gmx.de>
2022-10-19 13:27:20 +0200

    removed unused function

--- readDataGrabberLog.m
+++ readDataGrabberLog.m
@@ -1,11 +1,6 @@
 function [S, rawVarNames] = readDataGrabberLog(infile)
 % [S, rawVarNames] = readDataGrabberLog(infile)
 
-% TODO:
-% speedup with textscan:
-%   test = textscan(fid,'%d64 %{yyyy-MM-dd}D %{HH:mm:ss.SSS}D %f %f %f %f...
-%                        %f %f <USE AS MANY %f AS NEEDED!>','HeaderLines',1,'Delimiter',','); 
-
 REF_DATENUM = datenum('1970-01-01 00:00:00');
 I_FIRST_VAR = 5; % Time [ms],Date,Time,Rel.Time [s]
 MS2DAY = 1000*60*60*24;
@@ -39,36 +34,3 @@ end
 
 end
 
-
-
-function [nrows, nbyte] = getTxtFileLength(infile)
-%
-
-[fid, errMsg] = fopen(infile,'rt');
-assert(fid>0,'getTxtFileLength:CouldNotOpenFile','Could not open file ''%s''\n because ''%s''.',infile,errMsg);
-
-% get number of rows
-chunksize = 1e9; % read chuncks of 10MB at a time
-nrows = 0;
-while ~feof(fid)
-   ch = fread(fid, chunksize, '*uint8');
-   if isempty(ch)
-       break
-   end
-   nrows = nrows + sum(ch == 10);
-end
-% s = fread(fid,Inf,'*uint8');
-% nrows = sum(s==10);
-fseek(fid,-1,1);
-if fread(fid,1,'*uint8')~=10
-    nrows = nrows + 1;
-end
-
-% get size (bytes)
-if nargout>1
-    nbyte = ftell(fid);
-end
-
-fclose(fid);
-
-end
